e79bd2c48c66f598638dbd5f700ec8b8c48370db
jaehyun.yang <jaehyun.yang@jaehyunyangui-MacBookPro.local>
2021-06-03 18:16:50 +0900

    init

---
+++ src/views/Components/Components.js
@@ -0,0 +1,57 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Parallax from "components/Parallax/Parallax.js";
+// sections for this page
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import styles from "assets/jss/material-kit-react/views/components.js";
+
+import ProfileComponent from "views/ProfilePage/ProfilePage.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Components(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        brand="Material Kit React"
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="transparent"
+        changeColorOnScroll={{
+          height: 400,
+          color: "black",
+        }}
+        {...rest}
+      />
+      <Parallax image={require("assets/img/main.jpg").default}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem>
+              <div className={classes.brand}>
+                <h1 className={classes.title}>DevOps Dx Team</h1>
+                <h3 className={classes.subtitle}>
+                  A Badass Material-UI Kit based on Material Design.
+                </h3>
+              </div>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <ProfileComponent />
+      </div>
+    </div>
+  );
+}
